bde40a86f81b58f512475e4161b65c3f3ec2ea28
natepozar118 <103226153+natepozar118@users.noreply.github.com>
2022-08-30 16:40:45 -0700

    Add files via upload

---
+++ kwiter.js
@@ -0,0 +1,22 @@
+function addroom()
+{
+    room_name = document.getElementById("room_name").value;
+
+    firebase.database().ref("/").child(room_name).update({
+        purpose : "adding room name"
+    });
+
+        localStorage.setItem("room_name", room_name);
+
+        window.location = "kwiter_page.html";
+}
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) 
+{document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  =
+childSnapshot.key;
+       room_names = childKey;
+       console.log("Room Name - " + Room_names);
+       row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#"+ Room_names +"</div><hr>";
+       document.getElementById("output").innerHTML += row;
+            });});}
+       getData();
